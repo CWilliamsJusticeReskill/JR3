8306f5f5bc17a4262aea907be0f5b833c5c7e231
CWilliamsJusticeReskill <ChrisW@SccColorado.org>
2021-07-07 00:00:32 -0600

    pushing up Movie  Directory

--- Movie-Directory/script.js
+++ Movie-Directory/script.js
@@ -5,14 +5,57 @@
   .catch(err)
   console.log(err)
 get("http://www.omdbapi.com/?apikey=[98dcba67]&");*/
-fetch("http://www.omdbapi.com/?i=tt3896198&apikey=98dcba67&s=love")
-  .then(function (response) {
-    console.log(response);
-    //return response.json()
-  })
-  .then(function (data) {
-    console.log(data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+
+//var listedMovie = document.
+var button = document.getElementById("movieSearch");
+console.log(button);
+button.addEventListener("click", getData);
+function getData() {
+  var inputValue = document.getElementById("search").value;
+  var url = "http://www.omdbapi.com/?apikey=98dcba67&s=" + inputValue;
+  fetch(url)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.Search);
+      //var data = data.result;
+      var ul = document.getElementById("list");
+      for (var i = 0; i < data.Search.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.innerHTML = data.Search[i].Title;
+        ul.appendChild(createLi);
+      }
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+getData();
+
+function removeItems(li) {
+  document.getElementsByTagName(li);
+  for (var i = 0; i < li.length; i++) {
+    li.splice;
+  }
+}
+
+/*var unorderedList = document.createElement(ul);
+var element = document.getElementById("ul");
+element.appendChild(li);*/
+
+//var i = document.getElementById("input");
+//input.innerHTML= "inputValue";
+//var l = document.getElementById("movies");
+/* for (var i = 0; i > data.length; i++) {
+        console.log(data[i]);
+        //movies.innerHTML = "data[i]";
+        if (data[i].id === "") {
+          console.log([i]);
+        }
+      }
+    })*/
+
+//getData((l.innerHTML = document.getElementById("search")));
